b631ee94391d9025225761a98651f067e7c5b049
Victor Guan <78757246+VGuanCentra@users.noreply.github.com>
2024-06-13 17:42:38 -0700

    redirectUri Typo

--- src/msallib/msal/authConfig.js
+++ src/msallib/msal/authConfig.js
@@ -1,13 +1,14 @@
 const CLIENT_ID = "b1e9cca5-d8ef-4166-a20a-b2c42fec330a";
 export const API_SCOPE = "api://" + CLIENT_ID + "/api.scope";
-export const API_SCOPE_VGuanDemoScope = "api://" + CLIENT_ID + "/VGuanDemoScope";
+export const API_SCOPE_VGuanDemoScope =
+  "api://" + CLIENT_ID + "/VGuanDemoScope";
 
 export const msalConfig = {
   auth: {
     clientId: CLIENT_ID,
     authority:
       "https://login.microsoftonline.com/7c9bfdbe-55d0-49a9-b43e-c01229dc857e",
-    redirectUri: "https://centra-customer-service.vecel.app/",
+    redirectUri: "https://centra-customer-service.vercel.app/",
     // redirectUri: "http://localhost:3000/",
     postLogoutRedirectUri: "/",
     scope: API_SCOPE,
@@ -25,9 +26,9 @@ export const loginRequest = {
 };
 
 export const userDataLoginRequest = {
-    scopes: ["user.read"]
+  scopes: ["user.read"],
 };
 
 export const graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
 };
